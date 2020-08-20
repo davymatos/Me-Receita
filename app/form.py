40e9d6c2e4c6f56c46f75e4a3eabe67e3a18b9d4
Davy de Matos <davymatos01@gmail.com>
2020-08-19 21:37:38 -0300

    Modificação no form de nova receita

--- app/form.py
+++ app/form.py
@@ -7,7 +7,8 @@ from .models import Receita
 class ReceitaForm(ModelForm):
     class Meta:
         model = Receita
-        fields = ['nome', 'ingredientes', 'preparo', 'categoria']
+        fields = ['nome', 'ingredientes', 'preparo', 'categoria', 'usuario']
+        usuario = forms.CharField(label='usuario', max_length=100)
 
 
 class LoginForm(forms.Form):
